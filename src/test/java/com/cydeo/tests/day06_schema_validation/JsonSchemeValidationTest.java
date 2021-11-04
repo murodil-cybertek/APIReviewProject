e2c1de267e8a1a78ad8d5db06492f0d8c3c4f8e3
Murodil <murodil@cybertekschool.com>
2021-11-03 21:02:07 -0400

    Token generator util added

--- src/test/java/com/cydeo/tests/day06_schema_validation/JsonSchemeValidationTest.java
+++ src/test/java/com/cydeo/tests/day06_schema_validation/JsonSchemeValidationTest.java
@@ -1,10 +1,12 @@
 package com.cydeo.tests.day06_schema_validation;
 
+import io.restassured.response.Response;
 import org.junit.Test;
+import static io.restassured.RestAssured.*;
 
 public class JsonSchemeValidationTest extends LibraryApiTestBase{
     @Test
     public void getUserByIdSchemaValidation() {
-
+        //Response response = given()
     }
 }
---
+++ src/test/java/com/cydeo/tests/utils/TokenGenerator.java
@@ -0,0 +1,13 @@
+package com.cydeo.tests.utils;
+
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class TokenGenerator {
+    public static String getAccessToken(String email, String password) {
+        Response response = given().formParams("email", email, "password", password)
+                .when().post("/login");
+        return response.jsonPath().getString("token");
+    }
+}
